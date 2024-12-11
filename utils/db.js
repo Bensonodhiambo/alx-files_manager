fddb3cf92d806dee8335637d41876be32a35a0b0
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-12-11 13:18:40 -0800

    Task1

---
+++ utils/db.js
@@ -0,0 +1,54 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+        console.log('Connected to MongoDB');
+      })
+      .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+      });
+  }
+
+  isAlive() {
+    return this.client && this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const usersCollection = this.db.collection('users');
+      return await usersCollection.countDocuments();
+    } catch (err) {
+      console.error('Error getting user count:', err);
+      return 0;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const filesCollection = this.db.collection('files');
+      return await filesCollection.countDocuments();
+    } catch (err) {
+      console.error('Error getting file count:', err);
+      return 0;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
